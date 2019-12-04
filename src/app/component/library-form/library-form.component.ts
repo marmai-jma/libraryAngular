135b14388dc6f4d650877d1b52e12ad964e234a3
marmai-jma <johane.marmai@gmail.com>
2019-12-04 14:28:44 +0100

    Persistance en base h2 via post in libraryService

--- src/app/component/library-form/library-form.component.ts
+++ src/app/component/library-form/library-form.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { typesValidator } from 'src/app/directives/type-validator.directive';
+import { LibraryDTO } from 'src/app/shared-data/library-dto';
+import { DirectorDTO } from 'src/app/shared-data/director-dto';
+import { AdressDTO } from 'src/app/shared-data/adress-dto';
+import { LibraryService } from 'src/app/services/library.service';
+
 
 @Component({
   selector: 'app-library-form',
@@ -20,13 +25,24 @@ export class LibraryFormComponent implements OnInit {
     street: new FormControl(''),
   });
 
-  constructor() { }
+  // Pour injecter un service, il est impératif de mettre dans le constructor en private le service qu'on veut injecter
+  constructor(private libraryService: LibraryService) { }
 
   ngOnInit() {
   }
 
   onSubmit(){
     console.warn(this.libraryForm.value);
+    const libraryDTO = new LibraryDTO(null, this.libraryForm.value.label, this.libraryForm.value.type,
+      new AdressDTO (this.libraryForm.value.city, this.libraryForm.value.numberStreet,
+        this.libraryForm.value.postalCode, this.libraryForm.value.street),
+      new DirectorDTO(this.libraryForm.value.firstName, this.libraryForm.value.lastName));
+
+    console.log(libraryDTO);
+
+    this.libraryService.addLibrary(libraryDTO).subscribe(() => {
+        console.log('Success');
+      });
   }
 
   get city(){return this.libraryForm.get('city');}
--- src/app/services/library.service.ts
+++ src/app/services/library.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { LibraryDTO } from '../shared-data/library-dto';
 
@@ -7,11 +7,19 @@ import { LibraryDTO } from '../shared-data/library-dto';
   providedIn: 'root'
 })
 export class LibraryService {
+  httpOptions = {
+    headers: new HttpHeaders({'Content-type' : 'application/json'})
+  };
 
   constructor(private http: HttpClient) { }
 
   // Observable
   getAllLibraries(): Observable<LibraryDTO[]> {
     return this.http.get<LibraryDTO[]>('http://localhost:8080/libraries');
+  }
+
+  // POST : add a new libraryto the server
+  addLibrary(libraryDto : LibraryDTO): Observable<string> {
+    return this.http.post<string>('http://localhost:8080/libraries',libraryDto, this.httpOptions);
   }
 }
